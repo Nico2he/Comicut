594e53be227bd93b23b822f39d4e4944f7ef5552
Nico2he <nicoassesin@gmail.com>
2023-02-06 10:07:45 +0100

    Segundo Commit

--- app/src/main/java/com/nicorodgon/comicut/MainActivity.kt
+++ app/src/main/java/com/nicorodgon/comicut/MainActivity.kt
@@ -5,12 +5,12 @@ import android.view.Menu
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
 import com.nicorodgon.comicut.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -33,11 +33,9 @@ class MainActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.login
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
@@ -45,7 +43,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
---
+++ app/src/main/java/com/nicorodgon/comicut/ui/ActivityResultLauncher.kt
@@ -0,0 +1,113 @@
+package com.nicorodgon.comicut.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ActionCodeSettings
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.AuthUI.IdpConfig
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+import com.nicorodgon.comicut.R
+
+abstract class ActivityResultLauncher: AppCompatActivity() {
+
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_firebase_ui)
+    }
+
+    private fun createSignInIntent() {
+        val providers = arrayListOf(
+            IdpConfig.EmailBuilder().build(),
+            IdpConfig.PhoneBuilder().build(),
+            IdpConfig.GoogleBuilder().build(),
+            IdpConfig.FacebookBuilder().build(),
+            IdpConfig.TwitterBuilder().build())
+
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            val user = FirebaseAuth.getInstance().currentUser
+        } else {
+        }
+    }
+
+    private fun signOut() {
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener {
+            }
+    }
+
+    private fun delete() {
+        AuthUI.getInstance()
+            .delete(this)
+            .addOnCompleteListener {
+            }
+    }
+
+    private fun privacyAndTerms() {
+        val providers = emptyList<IdpConfig>()
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .setTosAndPrivacyPolicyUrls(
+                "https://example.com/terms.html",
+                "https://example.com/privacy.html")
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+
+    open fun emailLink() {
+        val actionCodeSettings = ActionCodeSettings.newBuilder()
+            .setAndroidPackageName(
+                "...",
+                true,
+                null)
+            .setHandleCodeInApp(true) // This must be set to true
+            .setUrl("https://google.com") // This URL needs to be whitelisted
+            .build()
+
+        val providers = listOf(
+            IdpConfig.EmailBuilder()
+                .enableEmailLinkSignIn()
+                .setActionCodeSettings(actionCodeSettings)
+                .build()
+        )
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+
+    open fun catchEmailLink() {
+        val providers: List<IdpConfig> = emptyList()
+        if (AuthUI.canHandleIntent(intent)) {
+            val extras = intent.extras ?: return
+            val link = extras.getString("email_link_sign_in")
+            if (link != null) {
+                val signInIntent = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setEmailLink(link)
+                    .setAvailableProviders(providers)
+                    .build()
+                signInLauncher.launch(signInIntent)
+            }
+        }
+    }
+}
